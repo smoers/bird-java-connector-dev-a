d6640d2997c554a501fdb0ce31a71783f19a09e4
smoers <serge.moers@gmail.com>
2015-06-19 16:43:45 +0200

    Sabena

--- src/org/bird/db/sessions/InfinispanSession.java
+++ src/org/bird/db/sessions/InfinispanSession.java
@@ -1,15 +1,28 @@
 package org.bird.db.sessions;
 
 import java.io.File;
+import java.lang.annotation.ElementType;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Properties;
 import java.util.UUID;
 
 import org.bird.db.connectors.IConnector;
 import org.bird.db.connectors.InfinispanConnector;
 import org.bird.db.exceptions.DbException;
+import org.bird.db.object.IAuthor;
+import org.bird.db.object.infinispan.AuthorInfinispan;
+import org.bird.db.object.infinispan.BookInfinispan;
+import org.bird.db.object.infinispan.CycleInfinispan;
 import org.bird.system.Core;
+import org.hibernate.search.cfg.Environment;
+import org.hibernate.search.cfg.SearchMapping;
 import org.infinispan.Cache;
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.Index;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
 
 public class InfinispanSession implements ISession {
 	
@@ -70,8 +83,44 @@ public class InfinispanSession implements ISession {
 
 	public Cache getCache(){
 		
-		Cache cache<
+		EmbeddedCacheManager manager = new DefaultCacheManager();
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		SearchMapping mapping = new SearchMapping();
 		
-		return null;
+		mapping.entity(AuthorInfinispan.class).indexed().providedId()
+		.property("lastname", ElementType.METHOD).field()
+		.property("firstname", ElementType.METHOD).field()
+		.property("authoralias", ElementType.METHOD).field();
+
+	
+		mapping.entity(CycleInfinispan.class).indexed().providedId()
+		.property("cycletitle", ElementType.METHOD).field();
+	
+		mapping.entity(BookInfinispan.class).indexed().providedId()
+		.property("title", ElementType.METHOD).field()
+		.property("style", ElementType.METHOD).field();
+				
+		Properties properties = new Properties();
+		properties.put(Environment.MODEL_MAPPING, mapping);
+	
+		Configuration c = cb.indexing()
+				.index(Index.LOCAL)
+				.withProperties(properties)
+				.persistence()
+				.passivation(false)
+				.addSingleFileStore()
+				.preload(true)
+				.shared(false)
+				.fetchPersistentState(true)
+				.ignoreModifications(false)
+				.purgeOnStartup(false)
+				.location("D:/infinispan")
+				.build();
+	
+		manager.defineConfiguration("book012015", c);
+	
+		Cache<UUID, IAuthor> cache = manager.getCache("book012015"); 
+
+		return cache;
 	}
 }
